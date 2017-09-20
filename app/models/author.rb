39a58b74fcb28d7ce06b157fc234a805e98f4d03
Katarzyna Jaskolska <katarzyna.jaskolska@netguru.pl>
2017-09-20 20:40:22 +0200

    Add validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,9 @@
 class Author < ApplicationRecord
   has_many :books
+
+  validates :first_name, presence: true, length: { maximum: 150 }
+  validates :last_name, presence: true, length: { maximum: 150 }
+  validates :bio, presence: true, length: { maximum: 1000 }
+  validates :birthday, presence: true
+  validates :nationality, presence: true, length: { maximum: 150 }
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
   belongs_to :author
+
+  validates :title, presence: true, length: { maximum: 150 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :published_at, presence: true
 end
