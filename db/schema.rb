7e8bc14cde86e50378f34bcba1eb1656739ebe79
Katarzyna Jaskolska <katarzyna.jaskolska@netguru.pl>
2017-10-12 18:47:41 +0200

    Add Rate model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920182733) do
+ActiveRecord::Schema.define(version: 20171012164349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(version: 20170920182733) do
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
+  create_table "rates", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "book_id"
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_rates_on_book_id"
+    t.index ["user_id"], name: "index_rates_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -66,4 +76,6 @@ ActiveRecord::Schema.define(version: 20170920182733) do
   add_foreign_key "authentication_tokens", "users"
   add_foreign_key "books", "authors"
   add_foreign_key "books", "users"
+  add_foreign_key "rates", "books"
+  add_foreign_key "rates", "users"
 end
---
+++ spec/factories/rates.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :rate do
+    user nil
+    book nil
+  end
+end
