b252046da0890512ac31532326befbdb64de3310
Katarzyna Jaskolska <katarzyna.jaskolska@netguru.pl>
2017-10-12 19:16:03 +0200

    Remove unnecessary rate_id

--- app/api/rates.rb
+++ app/api/rates.rb
@@ -18,11 +18,10 @@ class Rates < Grape::API
 
     desc 'Update book rate'
     params do
-      requires :rate_id, type: Integer, desc: 'Book id'
       requires :value, type: Integer, desc: 'Rate value from 1 to 5'
     end
     put ':id' do
-      current_user.rates.find(params[:rate_id]).update(value: params[:value])
+      current_user.rates.find(params[:id]).update(value: params[:value])
     end
 
     desc 'Delete book rate'
