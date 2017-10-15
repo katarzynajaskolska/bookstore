44fb5b54e943b16977eeaffc932c749fccb07471
Katarzyna Jaskolska <katarzyna.jaskolska@netguru.pl>
2017-10-15 12:00:56 +0200

    Add image url to books

--- app/api/entities/books_entity.rb
+++ app/api/entities/books_entity.rb
@@ -1,6 +1,6 @@
 module Entities
   class BooksEntity < Grape::Entity
-    expose :id, :title, :published_at, :author, :user_rate
+    expose :id, :title, :published_at, :author, :user_rate, :image_url
 
     def author
       "#{object.author.first_name} #{object.author.last_name}"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012164349) do
+ActiveRecord::Schema.define(version: 20171015095135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20171012164349) do
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["author_id"], name: "index_books_on_author_id"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
